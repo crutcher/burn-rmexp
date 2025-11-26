4b6b5e50e897a4aa419a6ade1722fce1da9fd37c
Crutcher Dunnavant <crutcher@gmail.com>
2025-11-26 10:45:00 -0800

    wip

--- crates/tbs-common/src/tensor_provider/mod.rs
+++ crates/tbs-common/src/tensor_provider/mod.rs
@@ -1,11 +1,33 @@
 //! # Tensor Provider
 
-use burn::tensor::{Slice, TensorData, TensorKind};
+use burn::tensor::{BasicOps, Slice, TensorData};
 use std::future::Future;
-use burn::prelude::Backend;
+use burn::prelude::{Backend, Bool, Float, Int};
 use burn::Tensor;
 
 pub mod metadata;
+
+pub enum FlatTensorWrapper<B: Backend> {
+    Float(Tensor<B, 1, Float>),
+    Int(Tensor<B, 1, Int>),
+    Bool(Tensor<B, 1, Bool>),
+}
+
+impl<B: Backend> From<Tensor<B, 1, Float>> for FlatTensorWrapper<B> {
+    fn from(value: Tensor<B, 1, Float>) -> Self {
+        FlatTensorWrapper::Float(value)
+    }
+}
+impl<B: Backend> From<Tensor<B, 1, Int>> for FlatTensorWrapper<B> {
+    fn from(value: Tensor<B, 1, Int>) -> Self {
+        FlatTensorWrapper::Int(value)
+    }
+}
+impl<B: Backend> From<Tensor<B, 1, Bool>> for FlatTensorWrapper<B> {
+    fn from(value: Tensor<B, 1, Bool>) -> Self {
+        FlatTensorWrapper::Bool(value)
+    }
+}
 
 pub enum TensorProviderError {
     InvalidShape,
@@ -14,33 +36,54 @@ pub enum TensorProviderError {
     InternalError,
 }
 
-pub trait TensorProvider {
+pub trait TensorProvider<B: Backend> {
     fn typed_shape(&self) -> metadata::TypedShape;
 
     fn get_slice_data(&self, slice: Slice) -> impl Future<Output=Result<TensorData, TensorProviderError>> + Send;
 
-    fn get_flat_slice_tensor<B, K>(&self, slice: Slice)
-    -> impl Future<Output=Result<Tensor<B, 1, K>, TensorProviderError>> + Send
-    where B: Backend,
-          K: TensorKind<B>;
+    fn get_flat_slice_tensor(&self, slice: Slice)
+    -> impl Future<Output=Result<FlatTensorWrapper<B>, TensorProviderError>> + Send;
 }
 
 pub struct ConstantTensorProvider<B, const D: usize, K>
-where B: Backend, K: TensorKind<B>{
-    tensor: Tensor<B, D, K>,
+where B: Backend, K: BasicOps<B>{
+    pub tensor: Tensor<B, D, K>,
 }
 
 impl<B, const D: usize, K> ConstantTensorProvider<B, D, K>
-where B: Backend, K: TensorKind<B> {
+where B: Backend, K: BasicOps<B> {
     pub fn new(tensor: Tensor<B, D, K>) -> Self {
         Self { tensor }
     }
 }
 
-impl<B, const D: usize, K> TensorProvider for ConstantTensorProvider<B, D, K>
-where B: Backend, K: TensorKind<B> {
+pub fn flatten<B, const D: usize, K>(tensor: Tensor<B, D, K>) -> Tensor<B, 1, K>
+where B: Backend, K: BasicOps<B>
+{
+    let num_elem = tensor.shape().num_elements();
+    tensor.reshape([num_elem])
+}
+
+impl<B, const D: usize, K> TensorProvider<B> for ConstantTensorProvider<B, D, K>
+where B: Backend, K: BasicOps<B> {
     fn typed_shape(&self) -> metadata::TypedShape {
         metadata::TypedShape { dtype: self.tensor.dtype(), shape: self.tensor.shape() }
+    }
+
+    fn get_slice_data(&self, slice: Slice) -> impl Future<Output=Result<TensorData, TensorProviderError>> + Send {
+        async move {
+            // TODO: check slice bounds
+            Ok(self.tensor.clone().slice(slice).into_data())
+        }
+    }
+
+    fn get_flat_slice_tensor(&self, slice: Slice) -> impl Future<Output=Result<FlatTensorWrapper<B>, TensorProviderError>> + Send {
+        async move {
+            // TODO: check slice bounds
+            let tensor = self.tensor.clone().slice(slice);
+            let _tensor: Tensor<B, 1, K> = flatten(tensor);
+            todo!()
+        }
     }
 }
 
